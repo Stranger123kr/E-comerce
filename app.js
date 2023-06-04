84bf5b94ef985e7ee2a175b8693c5a3e06285d00
nitesh <nitesh8825kr@gmail.com>
2023-06-04 15:07:27 +0530

    remove cursor effect

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 const menu_icon = document.querySelector(".menu_icon");
 const Menu_items = document.querySelector("#Menu_items");
-const cursor = document.querySelector("#cursor");
 
 // ============================ this is a mobile navbar effect ==================================
 
 menu_icon.addEventListener("click", () => {
   Menu_items.classList.toggle("menu_items_show");
 });
-
-// ============================ this is a cursor effect ==================================
-
-window.onmouseover = function (e) {
-  cursor.style.left = e.pageX - 25 + "px";
-  cursor.style.top = e.pageY - 25 + "px";
-  cursor.style.display = "block";
-};

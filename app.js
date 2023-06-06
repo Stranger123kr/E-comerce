a67b073fb15e83f07de74173aed8f7731b122cbd
nitesh <nitesh8825kr@gmail.com>
2023-06-06 12:53:29 +0530

    I have add new products.html file

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const menu_icon = document.querySelector(".menu_icon");
 const Menu_items = document.querySelector("#Menu_items");
+const All_img = document.querySelectorAll(".small_img_col img");
+const singe_img = document.querySelector(".singe_img");
+const range = document.getElementById("range");
 
 // ============================ this is a mobile navbar effect ==================================
 
 menu_icon.addEventListener("click", () => {
   Menu_items.classList.toggle("menu_items_show");
+});
+
+// ============================ this is a Image Select effect ==================================
+
+All_img.forEach((e) => {
+  e.addEventListener("click", Get_src);
+});
+
+function Get_src(src) {
+  singe_img.setAttribute("src", src.target.src);
+}
+
+// ============================ this is input correction ==================================
+
+range.addEventListener("click", () => {
+  range.value < 1 ? (range.value = 1) : range.value;
+  console.log(range.value)
 });
